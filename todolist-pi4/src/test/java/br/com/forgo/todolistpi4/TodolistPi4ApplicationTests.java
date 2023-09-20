6e443a9c01b6e75b4704cc677ba16f126a5136c4
Caio Henrique <cahenromar@gmail.com>
2023-09-19 21:50:38 -0300

    Criando o projeto To Do List

---
+++ todolist-pi4/src/test/java/br/com/forgo/todolistpi4/TodolistPi4ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.forgo.todolistpi4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistPi4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
